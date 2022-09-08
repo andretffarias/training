bf8713195b988b93ab20be2c7bf1a9bfc7e7120e
andré farias <102553282+andretffarias@users.noreply.github.com>
2022-09-08 18:15:45 -0300

    atualizacao 1.2

--- projeto02/script.js
+++ projeto02/script.js
@@ -1,32 +1,64 @@
-    const btn = document.querySelector('#send');
-    const res = document.querySelector('#res');
     
-    btn.addEventListener("click", function() {
+const btn = document.querySelector('#send');
+var res = document.querySelector('#res');
 
-        // ano do form
-        const anof = window.document.getElementById('vano')
-        const valueano = anof.value;
 
-        // ano atual
-        const data = new Date()
-        const anoa = data.getFullYear()
-        
-        // calculo da idade
-        const idade = anoa - valueano
-        
+res.innerHTML = `<p>Preencha os dados a cima para ver o resultado.</p>`
 
-        // genero
-        const sexm = document.querySelector('#sexm');
-        const sexf = document.querySelector('#sexf');
 
-        if (sexm.checked == true) {
-            switch (idade) {
-                case  false || anoa || idade <= 0:
-                    window.alert('[ERRO] tente verificar os dados')
-                break
-                default:
-            }
-        }else if (sexf.checked == true) {
-            
+btn.addEventListener("click", function (event) {
+
+    event.preventDefault()
+
+    // ano do form
+    const anof = window.document.getElementById('vano')
+    const valueano = anof.value;
+
+    // ano atual
+    const data = new Date()
+    const anoa = data.getFullYear()
+    
+    // calculo da idade
+    const idade = valueano
+
+    // genero
+    const sexm = document.querySelector('#sexm');
+    const sexf = document.querySelector('#sexf');
+
+    if (sexm.checked == true) {
+        if (idade < 0 || idade == false) {
+            window.alert("[ERRO] tente preencher os dados corretamente")
+        }else if (idade <= 3) {
+            res.innerHTML = `<img src="src/masculino/bebe(m).png" alt="img-bebe">`        
+        }else if (idade <= 13) {
+            res.innerHTML = `<img src="src/masculino/menino.png" alt="img-menino">`
+        }else if (idade <= 19) {
+            res.innerHTML = `<img src="src/masculino/adolescente(m).png" alt="img-adolecente">`
+        }else if (idade <= 29) {
+            res.innerHTML = `<img src="src/masculino/homem.png" alt="img-homem">`
+        }else if (idade <= 50) {
+            res.innerHTML= `<img src="src/masculino/homem-meia-idade.png" alt="img-homem">`
+        }else {
+            res.innerHTML = `<img src="src/masculino/idoso.png" alt="img-idoso">`
         }
-    })
+
+    }else if (sexf.checked == true) {
+        if (idade < 0 || idade == false) {
+            window.alert("[ERRO] tente preencher os dados corretamente")
+        }else if (idade <= 3) {
+            res.innerHTML = `<img src="src/feminino/bebe(f).png" alt="img-bebe">`        
+        }else if (idade <= 13) {
+            res.innerHTML = `<img src="src/feminino/menina.png" alt="img-menina">`
+        }else if (idade <= 19) {
+            res.innerHTML = `<img src="src/feminino/adolescente(f).png" alt="img-adolecente">`
+        }else if (idade <= 29) {
+            res.innerHTML = `<img src="src/feminino/mulher.png" alt="img-mulher">`
+        }else if (idade <= 50) {
+            res.innerHTML= `<img src="src/feminino/mulher-meia-idade.png" alt="img-mulher">`
+        }else {
+            res.innerHTML = `<img src="src/feminino/idosa.png" alt="img-idosa">`
+        }
+    }
+    
+    //res.innerHTML = `<p>Você tem ${idade}<p/>`
+})
