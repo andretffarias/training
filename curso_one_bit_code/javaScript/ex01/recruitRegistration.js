1bad920406bef408ab1b02b88a68bc463d83fd06
andré farias <102553282+andretffarias@users.noreply.github.com>
2023-01-18 16:51:54 -0300

    18/01/2023

---
+++ curso_one_bit_code/javaScript/ex01/recruitRegistration.js
@@ -0,0 +1,13 @@
+function processes() {
+
+    let name = document.getElementById("firstName")
+    let surname = document.getElementById("lastName")
+    let fieldOfStudy = document.getElementById("fielOfStudy")
+    let yearOfBirth = document.getElementById("yearOfBirth")
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let age = yearOfbirth - year
+
+    document.innerHTML = `<p>nome</p>`
+    
+}
