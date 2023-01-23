aefcd63c270cc1b376e3de756bbf33da0b90d3a3
andré farias <102553282+andretffarias@users.noreply.github.com>
2023-01-23 15:39:35 -0300

    tourism company exercise

--- curso_one_bit_code/javaScript/ex07/processe.js
+++ curso_one_bit_code/javaScript/ex07/processe.js
@@ -1,11 +1,26 @@
 const name = prompt("What your name?")
-const cities = confirm("Have you ever visited any city?")
+var cities = confirm("Have you ever visited any city?")
 
-
-while (cities == true) {
-    alert('rep')
-    var confirm3 = confirm("Have you visited any other cities?")
-    if (confirm3 == false) {
-        cities = false
+if (cities == true) {
+    var nameCities = prompt("which city did you visit")
+    var confirm01 = confirm("Have you visited any other cities?")
+    if (confirm01 == true) {
+        while (cities == true) {
+            var nameCities = nameCities + ", " + prompt("which city did you visit")
+            var confirm02 = confirm("Have you visited any other cities?")
+            if (confirm02 == false) {
+                cities = false
+            }
+        }
     }
+    alert(
+        "Name: " + name + "\n" +
+        "\nName of cities visited:\n"
+        + nameCities + "."
+    )
+}else {
+    alert(
+        "Name: " + name +
+        "\n\nYou haven't even visited a city yet"
+    )
 }
