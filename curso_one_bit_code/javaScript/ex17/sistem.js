3473ba5118948ffbec7f3dd0c4e619c4a0ac8789
andré farias <102553282+andretffarias@users.noreply.github.com>
2023-05-08 17:09:34 -0300

    sistem vacancy 0.01

--- curso_one_bit_code/javaScript/ex17/sistem.js
+++ curso_one_bit_code/javaScript/ex17/sistem.js
@@ -1,5 +1,6 @@
 let option;
 let listVacancies = [];
+let vacancyList = ""
 
 function newVacancy (nameVacancy, descriptionVacancy, wageVacancy) {
     let Vacancy = {
@@ -7,14 +8,16 @@ function newVacancy (nameVacancy, descriptionVacancy, wageVacancy) {
         description: "",
         wage: "",
         candidates: [],
-        id: listVacancies.length() + 1
+        id : listVacancies.length
     }
     Vacancy.name = nameVacancy
     Vacancy.description = descriptionVacancy
     Vacancy.wage = wageVacancy
     listVacancies.push(Vacancy)
     return listVacancies
-} 
+}
+
+
 do{
     option = parseInt(prompt(
         "1- list available vacancies.\n" +
@@ -27,12 +30,25 @@ do{
 
     switch(option){
         case 1:
-            alert(listVacancies)
+            if (listVacancies.length > 0) {
+                for (let i = 0; i < listVacancies.length; i++) {
+                    vacancy = listVacancies[i]
+                    vacancyList += "\nID: " + vacancy.id +
+                        "\nName: " + vacancy.name +
+                        "\nDescription: " + vacancy.description +
+                        "\nWage: $" + vacancy.wage + "/h\n" +
+                        "=============================================="
+                        "\n"
+                }
+                alert(vacancyList)
+            }else{
+                alert("There are no vacancies available.")
+            }
             break
         case 2:
             nameVacancy = prompt("Name of vacancy:")
             descriptionVacancy = prompt("Description of vacancy:")
-            wageVacancy = prompt("Wage of vacancy:")
+            wageVacancy = parseFloat(prompt("Wage of vacancy:"))
             newVacancy(nameVacancy, descriptionVacancy, wageVacancy)
             break
         case 3:
@@ -40,6 +56,7 @@ do{
         case 4:
             break
         case 5:
+            /// vai pedir o id da vaga em seguida pedir uma confirmacao de excluir a vaga se verdadeira usa o splice para remover a vaga da lista se nao retorna ao loop
             break
         case 6:
             alert("closing program...")
