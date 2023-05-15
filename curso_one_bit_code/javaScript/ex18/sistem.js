caf87aea7cb57bbea2aba18c27b17ac8f9b0125a
andré farias <102553282+andretffarias@users.noreply.github.com>
2023-05-15 18:49:54 -0300

    sistema de vagas versão 2 / 1.0

--- curso_one_bit_code/javaScript/ex18/sistem.js
+++ curso_one_bit_code/javaScript/ex18/sistem.js
@@ -1,7 +1,9 @@
 let vagas = [];
 let id = 1;
 let i = 0;
-let str = "";
+let iNum = 0;
+let strVagas = "";
+let strCandidatos = "";
 let idVaga = 0;
 let indiceVaga = 0;
 
@@ -48,34 +50,39 @@ function excluirVaga () {
         if (conf === true) {
             vagas.splice(indiceVaga, 1)
             alert("Vaga excluida.")
-            id++
         }else{alert("Vaga não excluida.")}
     }else{alert("Id não encontrado.")}
 }
 
 function verVagas () {
     for (i = 0; i < vagas.length; i++) {
-        str += 
+        strVagas += 
         "\nId: " + vagas[i].id +
         "\nNome: " + vagas[i].nome +
         "\nDescrição: " + vagas[i].descricao +
         "\nSalario: " + vagas[i].wege + 
-        "\nData: " + vagas[i].data
+        "\nData: " + vagas[i].data +
+        "\nCandidatos: " + vagas[i].candidatos.length
     }
-    alert(str)
+    alert(strVagas)
 }
 
 function verVaga () {
     idVaga = parseInt(prompt("Id da vaga:"))
     indiceVaga = vagas.findIndex(vaga => vaga.id === idVaga)
 
+    for (i = 0; vagas[indiceVaga].candidatos.length > i; i++) {
+        strCandidatos += iNum++ + "- " + vagas[indiceVaga].candidatos[i] + "\n"
+    }
+    
     if (indiceVaga != -1) {
         alert(
             "\nId: " + vagas[indiceVaga].id +
             "\nNome: " + vagas[indiceVaga].nome +
             "\nDescrição: " + vagas[indiceVaga].descricao +
             "\nSalario: " + vagas[indiceVaga].wege + 
-            "\nData: " + vagas[indiceVaga].data
+            "\nData: " + vagas[indiceVaga].data +
+            "\nCandidatos: " + strCandidatos
         )
     }else{alert("Nem uma vaga encontrada.")}
 }
@@ -89,10 +96,44 @@ function addCandidato () {
         conf = confirm(
             `Deseja add ${nomeCandidato} a vaga ${vagas[indiceVaga].nome}?`
         )
-
         if (conf === true) {
             vagas[indiceVaga].candidatos.push(nomeCandidato)
             alert("Candidato adicionado.")
         }else{alert("Candidato não adicionado.")}
     }else{alert("Vaga não encontrada.")}
 }
+
+
+
+do {
+    let option = prompt(
+        "\n1- Listar vagas disponíveis" +
+        "\n2- Visualizar uma vaga" +
+        "\n3- Inscrever um candidato em uma vaga" +
+        "\n4- Criar um nova vaga" +
+        "\n5- Excluir uma vaga" +
+        "\n6- Sair"
+    )
+
+    switch(option) {
+        case 1:
+            verVagas()
+            break
+        case 2:
+            verVaga()
+            break
+        case 3:
+            addCandidato()
+            break
+        case 4:
+            addVaga()
+            break
+        case 5:
+            excluirVaga()
+            break
+        case 6:
+            alert("Fechando programa.")
+            break
+        default: alert("[ERRO]: Opição invalida.")
+    }
+}while (option != 6)
