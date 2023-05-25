62ad9a6b3038f263841f7153d27e25d22cf02e31
andré farias <102553282+andretffarias@users.noreply.github.com>
2023-05-25 19:39:46 -0300

    agenda versao 1.0

--- curso_one_bit_code/D.O.M JS II/ex01/index.js
+++ curso_one_bit_code/D.O.M JS II/ex01/index.js
@@ -3,58 +3,71 @@ let i = 1;
 function addContact() {
     let contactSection = document.getElementById('contact-section')
 
-    let contactCreate = document.createElement("form")
-    contactCreate.setAttribute("id", "create-contact")
+    let formCreate = document.createElement("form")
+    formCreate.setAttribute("id", "create-contact")
 
     let labelName = document.createElement("label")
     labelName.setAttribute("for", "input-name")
     labelName.textContent = "Name:"
-
+    
     let inputName = document.createElement("input")
     inputName.setAttribute("id", "input-name")
     inputName.type = "text"
-
+    
     let br1 = document.createElement("br")
     let br2 = document.createElement("br")
-
+    
     let labelNum = document.createElement("label")
     labelNum.setAttribute("for", "input-num")
     labelNum.textContent = "Number:"
-
+    
     let inputNum = document.createElement("input")
     inputNum.setAttribute("id", "input-num")
     inputNum.type = "number"
-
-    let buttonCreate = document.createElement("button")
-    buttonCreate.setAttribute("onclick", "createForm(input-Name.value,input-num.value, contact-section)")
+    
+    let buttonCreate = document.createElement("input")
+    buttonCreate.type = "submit"
     buttonCreate.textContent = "create"
-
+    
     let buttonDelete = document.createElement("button")
     buttonDelete.setAttribute("onclick", "dellForm()")
     buttonDelete.textContent = "Delete"
+    
+    formCreate.append(labelName, inputName)
+    formCreate.appendChild(br1)
+    formCreate.append(labelNum, inputNum)
+    formCreate.appendChild(br2)
+    formCreate.append(buttonCreate, buttonDelete)
+    contactSection.append(formCreate)
 
-    contactCreate.append(labelName, inputName)
-    contactCreate.appendChild(br1)
-    contactCreate.append(labelNum, inputNum)
-    contactCreate.appendChild(br2)
-    contactCreate.append(buttonCreate, buttonDelete)
-    contactSection.append(contactCreate)
+    formCreate.addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        let name = inputName.value
+        let num = inputNum.value
+        console.log(name, num)
+        createContact(name, num, contactSection)
+    })
 }
 
-function createForm(name, num, contactSection) {
+function createContact(name, num, contactSection) {
     let divContact = document.createElement("div")
     divContact.setAttribute("id", "contact-div")
+    
+    let nameContact = document.createElement("h2")
+    nameContact.setAttribute("id", "name-contact")
+    nameContact.textContent = "- " + name
 
-    let h3 = document.createElement("h3")
-    h3.setAttribute("id", "name-contact")
-    h3.textContent = name
+    let p = document.createElement("p")
+    p.textContent = i
+    
+    let numContact = document.createElement("name")
+    numContact.setAttribute("id", "number-contact")
+    numContact.textContent = num
 
-    let h4 = document.createElement("h4")
-    h4.setAttribute("id", "number-contact")
-    h4.textContent = num
-
-    divContact.append(h3,h4)
+    divContact.append(p ,nameContact, numContact)
     contactSection.append(divContact)
+    i++
 }
 
 function dellForm() {
