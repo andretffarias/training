10e3a81712f162869a37c70174d3ccf406a791b1
andré farias <102553282+andretffarias@users.noreply.github.com>
2023-12-27 23:48:17 -0300

    27/12/23

--- projeto04/index.js
+++ projeto04/index.js
@@ -1,7 +1,45 @@
-function ADD(){
-    window.alert("add")
+function climb(){
+    //get inputs:
+    let ul = document.querySelector("#lineup-list")
+    let name = document.querySelector("#input-name").value
+    let shirtNum = document.querySelector("#input-shirt-number").value
+    let position = document.querySelector('input[name=radio-opition]:checked').value
+
+    //create tags:
+    let li = document.createElement('li')
+    let nameLabel = document.createElement('p')
+    let shirtNumLabel = document.createElement('p')
+    let positionLabel = document.createElement('p')
+
+    //seting tags:
+    nameLabel.innerText = "Name: "
+    shirtNumLabel.innerText = "Shirt number: "
+    positionLabel.innerText = "Position: "
+
+    nameLabel.style.fontWeight = "bold";
+    shirtNumLabel.style.fontWeight = "bold";
+    positionLabel.style.fontWeight = "bold";
+
+    //linking the tags:
+    li.append(nameLabel, name, shirtNumLabel, shirtNum, positionLabel, position)
+    ul.appendChild(li)
+
+    //calling fildsCleaning() function to clean the filds:
+    fieldsCleaning()
 }
 
-function REMOVE(){
+function remove(){
     window.alert("remove")
+}
+
+function fieldsCleaning(){
+    document.querySelector("#input-name").value = ""
+    document.querySelector("#input-shirt-number").value = ""
+    
+    let radioOptions = document.querySelectorAll('input[name=radio-opition]')
+    radioOptions.forEach(function (radioOptions) {
+        radioOptions.checked = false;
+    });
+
+    document.querySelector("#shirt-number-input-remove").value = ""
 }
