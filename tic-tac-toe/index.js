55395d96e3aaee1140396cd2048167da3d185d14
andré farias <102553282+andretffarias@users.noreply.github.com>
2024-02-06 22:31:17 -0300

    ttt

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -36,18 +36,8 @@ function game () {
     }else {
         appendDiv(2, 'Player 2', 'O', shiftStatus)
     }
-    
-    blocks.forEach(function (block) {
+    function turn (block) {
         const idDiv = block.id
-        const p = document.querySelector('#p-block-' + idDiv)
-        
-        p.innerText = ''
-        block.dataset.status = 'undefined'
-    })
-    
-    blocks.forEach(function turn (block) {
-        block.addEventListener('click', function () {
-            const idDiv = block.id
             const p = document.querySelector('#p-block-' + idDiv)
             
             if (p.innerText === '') {
@@ -55,8 +45,7 @@ function game () {
                     p.innerText = 'X'
                     block.dataset.status = 'x'
                     
-                    p.classList.remove('p1', 'p2')
-                    p.classList.add('p1')
+                    block.classList.add('p1')
                     
                     
                     shiftStatus  = 'p2'
@@ -65,34 +54,107 @@ function game () {
                     p.innerText = 'O'
                     block.dataset.status = 'o'
                     
-                    p.classList.remove('p1', 'p2')
-                    p.classList.add('p2')
+                    block.classList.add('p2')
                     
                     shiftStatus  = 'p1'
                     statusArrow.innerText = '<<<'
                 }
+                
+                
             }
-        })
-    })
+
+            const a1 = document.querySelector('#a1')
+            const b1 = document.querySelector('#b1')
+            const c1 = document.querySelector('#c1')
+            const a2 = document.querySelector('#a2')
+            const b2 = document.querySelector('#b2')
+            const c2 = document.querySelector('#c2')
+            const a3 = document.querySelector('#a3')
+            const b3 = document.querySelector('#b3')
+            const c3 = document.querySelector('#c3')
+
+            const a1stt = a1.dataset.status
+            const b1stt = b1.dataset.status
+            const c1stt = c1.dataset.status
+            const a2stt = a2.dataset.status
+            const b2stt = b2.dataset.status
+            const c2stt = c2.dataset.status
+            const a3stt = a3.dataset.status
+            const b3stt = b3.dataset.status
+            const c3stt = c3.dataset.status
+
+            if (a1stt === 'x' && a2stt === 'x' && a3stt === 'x') {
+                win('p1', a1, a2, a3)
+            }else if (b1stt === 'x' && b2stt === 'x' && b3stt === 'x') {
+                win('p1', b1, b2, b3)
+            }else if (c1stt === 'x' && c2stt === 'x' && c3stt === 'x') {
+                win('p1', c1, c2, c3)
+            }else if (a1stt === 'x' && b1stt === 'x' && c1stt === 'x') {
+                win('p1', a1, b1, c1)
+            }else if (a2stt === 'x' && b2stt === 'x' && c2stt === 'x') {
+                win('p1', a2, b2, c2)
+            }else if (a3stt === 'x' && b3stt === 'x' && c3stt === 'x') {
+                win('p1', a3, b3, c3)
+            }else if (a1stt === 'x' && b2stt === 'x' && c3stt === 'x') {
+                win('p1', a1, b2, c3)
+            }else if (c1stt === 'x' && b2stt === 'x' && a3stt === 'x') {
+                win('p1', c1, b2, a3)
+            }else if (a1stt === 'o' && a2stt === 'o' && a3stt === 'o') {
+                win('p2', a1, a2, a3)
+            }else if (b1stt === 'o' && b2stt === 'o' && b3stt === 'o') {
+                win('p2', b1, b2, b3)
+            }else if (c1stt === 'o' && c2stt === 'o' && c3stt === 'o') {
+                win('p2', c1, c2, c3)
+            }else if (a1stt === 'o' && b1stt === 'o' && c1stt === 'o') {
+                win('p2', a1, b1, c1)
+            }else if (a2stt === 'o' && b2stt === 'o' && c2stt === 'o') {
+                win('p2', a2, b2, c2)
+            }else if (a3stt === 'o' && b3stt === 'o' && c3stt === 'o') {
+                win('p2', a3, b3, c3)
+            }else if (a1stt === 'o' && b2stt === 'o' && c3stt === 'o') {
+                win('p2', a1, b2, c3)
+            }else if (c1stt === 'o' && b2stt === 'o' && a3stt === 'o') {
+                win('p2', c1, b2, a3)
+            }
+
+        }
+        function removeEv (block) {
+            block.removeEventListener('click', turn)
+        }
+    
+        function win (player, case1, case2, case3) {
+            blocks.forEach(function (block) {
+                removeEv(block)
+            })
+            case1.classList.add('win')
+            case2.classList.add('win')
+            case3.classList.add('win')
+            
+            case1.classList.remove('p1', 'p2')
+            case2.classList.remove('p1', 'p2')
+            case3.classList.remove('p1', 'p2')  
+        
+        }
+
 }
 
-function appendDiv (numberPlayer, pName, pLetter, shiftStatus) {
+
+function appendDiv (numberPlayer, pName, pLetter) {
     const div = document.createElement('div')
     div.id = 'div-p' + numberPlayer
     div.classList.add('div-player')
-
+    
     const name = document.createElement('p')
     name.id = 'name-p' + numberPlayer
     name.innerText = pName + ':'
-
+    
     const letter = document.createElement('p')
     letter.id = pLetter.toLowerCase()
     letter.innerText = pLetter
-
+    
     div.append(name, letter)
     sectionInfo.append(div)
     btnPlay.remove()
 }
-
 
 btnPlay.addEventListener('click', game)
