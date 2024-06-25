a9b02d6094c9c3029aaa85227ce5e4300c31115c
Andre <atffpc@gmail.com>
2024-06-25 13:21:49 -0300

    react ex03

---
+++ react/ex03/src/App.jsx
@@ -0,0 +1,13 @@
+import Title from "./componets/Title.jsx"
+import { Subtitle } from "./componets/Subtitle.jsx"
+import StatusText from "./componets/StatusText.jsx"
+
+export default function App () {
+  return (
+    <div>
+      <Title />
+      <Subtitle />
+      <StatusText />
+    </div>
+  )
+}
